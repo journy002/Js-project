53158cf9ce8da33fff5afab4d8f43eb497819eb0
journy002 <journy002@naver.com>
2021-12-17 16:26:14 +0900

    update

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-let navbar = document.getElementById('navbar');
-let navbar_menu = document.querySelector('.navbar__menu');
-let nav_height = navbar.getBoundingClientRect().height;
+'use strict'
+const navbar = document.getElementById('navbar');
+const nav_height = navbar.getBoundingClientRect().height;
+const navbar__menu = document.querySelector('.navbar__menu');
 
 document.addEventListener('scroll', () => {
     if (window.scrollY > nav_height) {
         navbar.classList.add('navbar--dark');
-        navbar_menu.classList.add('font--dark');
+        navbar__menu.classList.add('color--dark');
     } else {
         navbar.classList.remove('navbar--dark');
-        navbar_menu.classList.remove('font--dark');
+        navbar__menu.classList.remove('color--dark');
     }
 });
 
-navbar_menu.addEventListener('click', (event) => {
+navbar__menu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
-
-    if (link == null) {
-        return
-    }
-
+    if (link == null) return;
     scrollIntoView(link);
 });
 
@@ -27,5 +24,5 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({
         behavior: 'smooth'
-    })
-}
+    });
+};
